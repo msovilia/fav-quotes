f5b5b3ec1c1620ffc1f5caffd2d6d3e1ba5bf2ec
msovilia <ovilia.ssi@gmail.com>
2020-03-16 17:59:26 +0700

    first commit

---
+++ quotes.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:ovilia015@localhost/quotes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kozwcnodxaexyh:cafe2f97960994e9f748a7acc6bf47f236707cdece5eb69296bc189820e61aa6@ec2-23-22-156-110.compute-1.amazonaws.com:5432/degt6pm2v9h3m7'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db = SQLAlchemy(app)
+class Favquotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    quote = db.Column(db.String(2000))
+
+@app.route('/')
+def index():
+    result = Favquotes.query.all()
+    return render_template('index.html', result=result)
+
+@app.route('/quotes')
+def quotes():
+    return render_template('quotes.html')
+
+@app.route('/process', methods = ['POST'])
+def process():
+    author = request.form['author']
+    quote = request.form['quote']
+    quotedata = Favquotes(author=author, quote=quote)
+    db.session.add(quotedata)
+    db.session.commit()
+
+    return redirect(url_for('index'))
